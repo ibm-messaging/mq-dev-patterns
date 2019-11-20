78213f8587f8c0933bc8106fb18398f3de44f528
Soheel Chughtai <soheel_chughtai@uk.ibm.com>
2019-11-20 16:55:14 +0000

    accept queue manager not available

--- dotnet/dotNetGet.cs
+++ dotnet/dotNetGet.cs
@@ -84,6 +84,16 @@ namespace ibmmq_samples
                     Console.WriteLine("XMSException caught: {0}", ex);
                     Console.WriteLine("Error Code: {0}", ex.ErrorCode);
                     Console.WriteLine("Error Message: {0}", ex.Message);
+
+                    if (ex.LinkedException != null &&
+                           IBM.XMS.MQC.MQRC_Q_MGR_NOT_AVAILABLE.ToString().Equals(ex.LinkedException.Message))
+                    {
+                        Console.WriteLine("Queue Manager on this endpoint not available");
+                        Console.WriteLine("Moving onto next endpoint");
+                        continue;
+                    }
+                    Console.WriteLine("Unexpected Error - Aborting");
+                    throw;
                 }
             }
         }
