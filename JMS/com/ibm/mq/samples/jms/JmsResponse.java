460b0b4f8135e54ade4b1a7c3c6b663bf596d71f
Tanmay-Kudchadkar <98406947+Tanmay-Kudchadkar@users.noreply.github.com>
2022-05-10 15:55:47 +0100

    Update JmsResponse.java

--- JMS/com/ibm/mq/samples/jms/JmsResponse.java
+++ JMS/com/ibm/mq/samples/jms/JmsResponse.java
@@ -1,5 +1,5 @@
 /*
-* (c) Copyright IBM Corporation 2019
+* (c) Copyright IBM Corporation 2019, 2022
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
