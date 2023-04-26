368cb060c4fcb71f24aae6168cce3d256cf10efd
root <root@breadth1.fyre.ibm.com>
2023-04-26 07:13:09 -0700

    cleaning up

---
+++ OpenLiberty-MDB/producer/download-packages.sh
@@ -0,0 +1,3 @@
+ curl -o com.ibm.mq.allclient-9.3.0.0.jar https://repo1.maven.org/maven2/com/ibm/mq/com.ibm.mq.allclient/9.3.0.0/com.ibm.mq.allclient-9.3.0.0.jar
+ curl -o javax.jms-api-2.0.1.jar https://repo1.maven.org/maven2/javax/jms/javax.jms-api/2.0.1/javax.jms-api-2.0.1.jar
+ curl -o json-20220320.jar https://repo1.maven.org/maven2/org/json/json/20220320/json-20220320.jar
--- OpenLiberty-MDB/producer/jms-put.sh
+++ OpenLiberty-MDB/producer/jms-put.sh
@@ -1,2 +1,2 @@
 export PATH=$PATH:/home/jdk-11.0.16.1+1/bin
-java -cp ./com.ibm.mq.allclient-9.3.0.0.jar:./javax.jms-api-2.0.1.jar:./json-20220320.jar:. com.ibm.mq.samples.jms.JmsPut
+java -cp ./com.ibm.mq.allclient-9.3.0.0.jar:./javax.jms-api-2.0.1.jar:./json-20220320.jar:. com/ibm/mq/samples/jms/JmsPut.java
