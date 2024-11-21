7ba042b4fab8443abf4f0ed2f93cb2a4cb91dc5d
ibmmqmet <marke_taylor@uk.ibm.com>
2024-11-21 11:47:26 +0000

    add topic

---
+++ C/samplepublish.c
@@ -0,0 +1,149 @@
+/**
+ * Copyright 2024 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+/*
+ * This is a demonstration showing the Publish operation to a MQ Topic
+ * using the MQI C interface
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <cmqc.h>
+
+#include "common.h"
+#include "config.h"
+
+static int openTopic(MQHCONN hConn, PMQHOBJ pHObj);
+static int publishMessage(MQHCONN hConn, MQHOBJ hObj, char *msg);
+
+// There are no command line parameters to this program
+int main(int argc, char **argv) {
+  int rc = 0;
+  time_t now;
+  MQHCONN hConn = MQHC_UNUSABLE_HCONN;
+  MQHOBJ hObj = MQHO_UNUSABLE_HOBJ;
+
+   if (getenv(CONFIG_DEBUG)) {
+    debug = 1;
+  }
+
+  char *configFile = getenv(CONFIG_JSON_FILE);
+  if (!configFile) {
+    configFile = DEFAULT_CONFIG_FILE;
+  }
+
+  printf("Starting up Application: %s\n", argv[0]);
+  rc = parseConfig(configFile);
+  if (rc == 0) {
+    rc = connectQMgr(&hConn);
+  }
+
+  if (rc == 0) {
+    rc = openTopic(hConn, &hObj);
+  }
+
+  if (rc == 0) {
+    char msgData[DEFAULT_BUFFER_LENGTH];
+    time(&now);
+    // ctime always returns a 26-byte buffer. But we want to strip off the trailing "\n" and NUL.
+    // So force the buffer to use 24 chars.
+    sprintf(msgData, "Hello from C at %24.24s", ctime(&now));
+    rc = publishMessage(hConn, hObj, msgData);
+  }
+
+  if (hObj != MQHO_UNUSABLE_HOBJ) {
+    closeQueue(hConn, &hObj);
+  }
+
+  if (hConn != MQHC_UNUSABLE_HCONN) {
+    disconnectQMgr(&hConn);
+  }
+
+  printf("Done. Exit code:%d\n", rc);
+  exit(rc);
+}
+
+/*
+ * Open a topic for OUTPUT as we will be publishing a message
+ *
+ * Return 0 if OK; -1 otherwise
+ */
+static int openTopic(MQHCONN hConn, PMQHOBJ pHObj) {
+  MQLONG compCode;
+  MQLONG reason;
+  MQCHARV objectString = {MQCHARV_DEFAULT};
+  char *topic;
+  int rc = 0;
+
+  MQOD mqod = {MQOD_DEFAULT};
+  MQLONG options = MQOO_OUTPUT;
+
+  // The objectString contains the full topic string that we
+  // want to use. Alternatives allow a combination of administered object
+  // names and the objectString value, but that's more advanced.
+  topic = mqEndpoints[0].topicName;
+  objectString.VSPtr = topic;
+  objectString.VSLength = (MQLONG)strlen(topic);
+
+  mqod.Version = MQOD_VERSION_4; // The ObjectString field requires this level of structure
+  mqod.ObjectType = MQOT_TOPIC;
+  mqod.ObjectString = objectString;
+
+  MQOPEN(hConn, &mqod, options, pHObj, &compCode, &reason);
+
+  if (reason != MQRC_NONE) {
+    printError("MQOPEN", compCode, reason);
+    rc = -1;
+  }
+
+  return rc;
+}
+
+/*
+ * Publish the message on the topic
+ *
+ * Return 0 if OK; -1 otherwise
+ */
+static int publishMessage(MQHCONN hConn, MQHOBJ hObj, char *msg) {
+  MQLONG compCode;
+  MQLONG reason;
+  int rc = 0;
+  MQMD mqmd = {MQMD_DEFAULT};
+  MQPMO mqpmo = {MQPMO_DEFAULT};
+
+  // Options to control how the message is published. We might be interested
+  // to know if there are no subscribers.
+  mqpmo.Options = MQPMO_FAIL_IF_QUIESCING;
+  mqpmo.Options |= MQPMO_NO_SYNCPOINT;
+  mqpmo.Options |= MQPMO_NEW_MSG_ID;
+  mqpmo.Options |= MQPMO_WARN_IF_NO_SUBS_MATCHED;
+
+  // We are sending a character string. Set the format to indicate
+  // that, so that the recipient can convert the codepage if necessary
+  memcpy(mqmd.Format,MQFMT_STRING,MQ_FORMAT_LENGTH);
+
+  // Now publish the message
+  MQPUT(hConn, hObj, &mqmd, &mqpmo, strlen(msg), msg, &compCode, &reason);
+  if (reason != MQRC_NONE) {
+    printError("MQPUT", compCode, reason);
+    rc = -1;
+  }
+
+  return rc;
+}
---
+++ C/samplesubscribe.c
@@ -0,0 +1,181 @@
+/**
+ * Copyright 2024 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+/* This is a demonstration showing the GET operations from an MQ Queue
+ * using the MQI C interface.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <cmqc.h>
+
+#include "common.h"
+#include "config.h"
+
+static int subscribeTopic(MQHCONN hConn, PMQHOBJ pTopicHObj, PMQHOBJ pQueueHObj);
+static int getMessages(MQHCONN hConn, MQHOBJ hObj);
+
+#define WAIT_INTERVAL 3 // Seconds to wait for more publications
+
+// There are no command line parameters to this program
+int main(int argc, char **argv) {
+  int rc = 0;
+  time_t now;
+  MQHCONN hConn = MQHC_UNUSABLE_HCONN;
+  MQHOBJ topicHObj = MQHO_UNUSABLE_HOBJ;
+  MQHOBJ queueHObj = MQHO_UNUSABLE_HOBJ;
+
+  if (getenv(CONFIG_DEBUG)) {
+    debug = 1;
+  }
+
+  char *configFile = getenv(CONFIG_JSON_FILE);
+  if (!configFile) {
+    configFile = DEFAULT_CONFIG_FILE;
+  }
+
+  printf("Starting up Application: %s\n", argv[0]);
+  rc = parseConfig(configFile);
+  if (rc == 0) {
+    rc = connectQMgr(&hConn);
+  }
+
+  if (rc == 0) {
+    rc = subscribeTopic(hConn, &topicHObj, &queueHObj);
+  }
+  if (rc == 0) {
+    rc = getMessages(hConn, queueHObj);
+  }
+
+  if (queueHObj != MQHO_UNUSABLE_HOBJ) {
+    closeQueue(hConn, &queueHObj);
+  }
+
+  if (topicHObj != MQHO_UNUSABLE_HOBJ) {
+    closeQueue(hConn, &topicHObj);
+  }
+
+  if (hConn != MQHC_UNUSABLE_HCONN) {
+    disconnectQMgr(&hConn);
+  }
+
+  printf("Done. Exit code:%d\n", rc);
+  exit(rc);
+}
+
+/*
+ * Open a queue for INPUT as we will be getting messages
+ *
+ * Return 0 if OK; -1 otherwise
+ */
+static int subscribeTopic(MQHCONN hConn, PMQHOBJ pTopicHObj, PMQHOBJ pQueueHObj) {
+  MQLONG compCode;
+  MQLONG reason;
+  MQCHARV objectString = {MQCHARV_DEFAULT};
+  char *topic;
+
+  int rc = 0;
+
+  MQSD mqsd = {MQSD_DEFAULT};
+
+  mqsd.Options = MQSO_CREATE | MQSO_MANAGED;
+  mqsd.Options |= MQSO_NON_DURABLE;
+  mqsd.Options |= MQSO_FAIL_IF_QUIESCING;
+
+  // The objectString contains the full topic string that we
+  // want to use. Alternatives allow a combination of administered object
+  // names and the objectString value, but that's more advanced.
+  topic = mqEndpoints[0].topicName;
+  objectString.VSPtr = topic;
+  objectString.VSLength = (MQLONG)strlen(topic);
+
+  MQSUB(hConn, &mqsd, pQueueHObj, pTopicHObj, &compCode, &reason);
+
+  if (reason != MQRC_NONE) {
+    printError("MQSUB", compCode, reason);
+    rc = -1;
+  }
+
+  return rc;
+}
+
+/*
+ * Get all available messages from the queue, printing the contents.
+ * Return 0 if OK; -1 otherwise
+ */
+static int getMessages(MQHCONN hConn, MQHOBJ hObj) {
+
+  MQLONG compCode;
+  MQLONG reason;
+  int rc = 0;
+  int ok = 1;
+  MQMD mqmd = {MQMD_DEFAULT};
+  MQGMO mqgmo = {MQGMO_DEFAULT};
+  char buffer[DEFAULT_BUFFER_LENGTH];
+  MQLONG datalength;
+
+  // Structure version must be high enough to recognise the MatchOptions field
+  mqgmo.Version = MQGMO_VERSION_2;
+
+  // Various options to control retrieval
+  mqgmo.Options = MQGMO_NO_SYNCPOINT;
+  mqgmo.Options |= MQGMO_FAIL_IF_QUIESCING;
+  mqgmo.Options |= MQGMO_WAIT;
+  mqgmo.Options |= MQGMO_CONVERT;
+
+  mqgmo.Options |= MQGMO_ACCEPT_TRUNCATED_MSG; // Process the message even if it is too long for the buffer
+
+  mqgmo.WaitInterval = WAIT_INTERVAL * 1000;
+
+  // Not going to try to match on MsgId or CorrelId
+  mqgmo.MatchOptions = MQMO_NONE;
+
+  // Loop until there are no more messages on the queue
+  while (ok) {
+
+    MQGET(hConn, hObj, &mqmd, &mqgmo, sizeof(buffer), buffer, &datalength, &compCode, &reason);
+
+    if (reason == MQRC_NONE) {
+      if (!strncmp(mqmd.Format, MQFMT_STRING, MQ_FORMAT_LENGTH)) {
+        printf("Message: %*.*s\n", datalength, datalength, buffer);
+      } else {
+        char title[32];
+        sprintf(title, "Message Type:%*.*s", MQ_FORMAT_LENGTH, MQ_FORMAT_LENGTH, mqmd.Format);
+        dumpHex(title, buffer, datalength);
+      }
+    } else {
+      printError("MQGET", compCode, reason);
+      switch (reason) {
+      case MQRC_NO_MSG_AVAILABLE:
+        // This is not really an error but we do need to break from the loop
+        ok = 0;
+        break;
+      case MQRC_TRUNCATED_MSG_ACCEPTED:
+        // Carry on if there are more messages
+        break;
+      default:
+        rc = -1;
+        ok = 0;
+        break;
+      }
+    }
+  }
+
+  return rc;
+}
