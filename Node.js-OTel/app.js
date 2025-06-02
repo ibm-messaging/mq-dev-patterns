99e14f5f497812375a3dd8a00240d5c2e0ffc084
Soheel Chughtai <soheel_chughtai@uk.ibm.com>
2025-06-02 13:08:38 +0100

    Add in skeleton express code

--- Node.js-OTel/app.js
+++ Node.js-OTel/app.js
@@ -18,8 +18,44 @@
 const debug_info = require('debug')('mqsample:otel:app:info');
 const debug_warn = require('debug')('mqsample:otel::app:warn');
 
-
 debug_info('Application is starting');
+
+const express = require('express');
+
+const {appLimits} = require('./settings/limits.js');
+
+const HTTP_PORT = parseInt(process.env.PORT || '8080');
+const app = express();
+
+app.get('/put', (req, res) => {
+    debug_info('Put requested');
+    debug_info('Determining number of puts required');
+
+    let numPuts = determineNumInRequest(req);
+    debug_info(`Will be sending ${numPuts} messages`);
+
+    res.send(JSON.stringify(`Sending ${numPuts} messages`));
+    
+});
+
+function determineNumInRequest(req) {
+    let number = req.query.num ? parseInt(req.query.num.toString()) : NaN;
+    if (isNaN(number)) {
+        number = appLimits.randomNum();
+    } else {
+        debug_info("TEST 01");
+        number = appLimits.applyLimit(number);
+        debug_info("TEST 02");
+    }
+    return number;
+}
+
+app.listen(HTTP_PORT, () => {
+    debug_info(`Listening on http://localhost:${HTTP_PORT}`);
+})
+
+
+debug_info('Application is listening');
 
 
 debug_info('Application is ending');
