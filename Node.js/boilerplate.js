56d6140f52cba3187fef35bc56e81426a3481f33
Soheel Chughtai <soheel_chughtai@uk.ibm.com>
2025-07-03 14:31:25 +0100

    Only report a callback async error once

--- Node.js/boilerplate.js
+++ Node.js/boilerplate.js
@@ -43,6 +43,7 @@ var count = 0;
 var LIMIT = 5;
 var waitInterval = 10; // max seconds to wait for a new message
 var canExit = false;
+var last_mqrc = 0;
 var activeCB = null;
 var bpInstance = null;
 
@@ -840,7 +841,8 @@ class MQBoilerPlate {
     if (err) {
       if (err.mqrc == MQC.MQRC_NO_MSG_AVAILABLE) {
         debug_info("No more messages available.");
-      } else {
+      } else if (last_mqrc !== err.mqrc) {
+        last_mqrc = err.mqrc;
         MQBoilerPlate.reportError(err);
       }
       canExit = true;
