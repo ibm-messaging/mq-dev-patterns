943812684175de0b3080d9d983efcae48d5748e9
ibmmqmet <marke_taylor@uk.ibm.com>
2026-09-08 10:05:52 +0100

    Update SpringBoot-MVC demo * Update to current Spring Boot versions * Add README to describe demo * Add receive-timeout property

--- SpringBoot-MVC/src/main/resources/static/script.js
+++ SpringBoot-MVC/src/main/resources/static/script.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022, 2024 IBM Corp.
+ * Copyright 2022, 2026 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ const openMQConsole = () => {
 const performGetQMAddress = async () => {
   try {
     
-    const response = await fetch(`http://localhost:8080/address`);
+    const response = await fetch(`http://localhost:8088/address`);
 
     if (!response.ok) {
       return { status: false, data: "Some errors occured" };
@@ -50,7 +50,7 @@ const performGetQMAddress = async () => {
 const performSend = async (msg) => {
   try {
     const response = await fetch(
-      `http://localhost:8080/send?msg=${encodeURIComponent(msg)}`
+      `http://localhost:8088/send?msg=${encodeURIComponent(msg)}`
     );
 
     if (!response.ok) {
@@ -66,7 +66,7 @@ const performSend = async (msg) => {
 
 const performGet = async () => {
   try {
-    const response = await fetch(`http://localhost:8080/recv`);
+    const response = await fetch(`http://localhost:8088/recv`);
 
     if (!response.ok) {
       return { status: false, data: "Some errors occured" };
